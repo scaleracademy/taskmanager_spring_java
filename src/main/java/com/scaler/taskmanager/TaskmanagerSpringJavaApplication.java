0618adbe230e1cb7cbc95e3b6d86ec65ab2a42c1
Arnav Gupta <championswimmer@gmail.com>
2022-12-30 14:39:00 +0530

    initial project setup via Spring Initializr
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

---
+++ src/main/java/com/scaler/taskmanager/TaskmanagerSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerSpringJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanagerSpringJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/taskmanager/TaskmanagerSpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerSpringJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
